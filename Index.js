ea2b5d1b971b55feb28608c0f1f111b778b23b6b
Lassener01 <lasse.ner@hotmail.com>
2025-03-19 12:29:54 +0100

    Just trying random shit

--- Index.js
+++ Index.js
@@ -1,1 +1,8 @@
-var
+const hello = 'Hello and welcome to my course!';
+
+console.log(hello);
+
+let bitch;
+bitch= 'Not everyone can be profitable! But if you put in the work you put yourself over others!'
+
+console.log(bitch);
