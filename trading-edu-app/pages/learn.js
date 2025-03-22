6141e4a37213399ec6ecdab008532730967784d0
Lassener01 <lasse.ner@hotmail.com>
2025-03-22 19:52:22 +0100

    New file for testing and learning

---
+++ trading-edu-app/pages/learn.js
@@ -0,0 +1,9 @@
+export default function Learn() {
+    return (
+      <div style={{ padding: '40px', fontFamily: 'Arial' }}>
+        <h1 style={{ color: '#1e90ff' }}>Welcome to LazzFXs course!</h1>
+        <p>This will change youre life if you are serious!</p>
+      </div>
+    );
+  }
+       
